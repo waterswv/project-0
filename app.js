e00450b310717c9616460799f383701e5b2a1435
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-07-11 10:27:47 -0700

    added logic to calculate a winner and stop the program once a player crosses virtual finish line

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ $(document).ready(function() {
   var playerB = "images/scalene-triangle.png";
   var p1A = 2;
   var p2B = 2;
+  var hasWon = false;
 
   $('button').on('click', function() {
     $('#move-1B').replaceWith(`<img id="move-1B" alt="scalene" width="80" height="68" src=${playerB}>`); // add the triangle to the board
@@ -16,16 +17,29 @@ $(document).ready(function() {
 
   $(document).keydown(function(e) {
 
-    if (e.which == 80) { // 80 is the lowercase 'p' key
-    $(`#move-${p2B-1}B`).replaceWith(`<img src="">`)
-    $(`#move-${p2B}B`).replaceWith(`<img id="move-${p2B}B" alt="isosceles" width="68" height="80" src=${playerB}>`); // add the other triangle to the board
-      p2B += 1;
-    }
-    if (e.which == 81) { // 81 is the lowercase 'q' key
+    if (e.which == 80 && (p2B < 13) && (hasWon === false)) { // 80 is the lowercase 'p' key
+      $(`#move-${p2B-1}B`).replaceWith(`<img src="">`)
+      $(`#move-${p2B}B`).replaceWith(`<img id="move-${p2B}B" alt="isosceles" width="68" height="80" src=${playerB}>`); // move triangle to the next space
+      if (p2B < 12 && (hasWon === false)) {
+        p2B += 1;
+      } else {
+        console.log(p2B);
+        console.log("The scalene wins the game!")
+        hasWon = true;
+        
+      }
+    } else if (e.which == 81 && (p1A < 13) && (hasWon === false)) { // 81 is the lowercase 'q' key
       $(`#move-${p1A-1}A`).replaceWith(`<img src="">`)
-      $(`#move-${p1A}A`).replaceWith(`<img id="move-${p1A}A" alt="isosceles" width="68" height="80" src=${playerA}>`); // add the other triangle to the board
-      p1A += 1;
+      $(`#move-${p1A}A`).replaceWith(`<img id="move-${p1A}A" alt="isosceles" width="68" height="80" src=${playerA}>`); // move other triangle to the next space
       console.log(p1A);
+      if (p1A < 12 && (hasWon === false)) {
+        p1A += 1;
+      } else {
+        console.log(p1A);
+        console.log("The isosceles wins the game!")
+        hasWon = true;
+      }
+
     }
   });
 
