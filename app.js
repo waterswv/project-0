7a67d259b8414f88f87ffbc22e7f21d009d33a32
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-07-10 23:18:43 -0700

    added start button that kicks off game and places 'players' images at starting line

--- app.js
+++ app.js
@@ -4,13 +4,23 @@ $(document).ready(function() {
   // goes inside this function
   console.log("Sanity Check: DOM is Ready!");
 
-  function movePlayer () {
-  $('div').on('click', function (){
+  var playerA = "images/isosceles-triangle.png";
+  var playerB = "images/scalene-triangle.png";
 
-      $('#move-1A').empty();
-      $('#move-2A').replaceWith("<p id=move-2A>Move</p>");
-  });
-}
-movePlayer();
+$('button').on("click", function(){
+  //  movePlayer();
+    $('#move-1A').replaceWith(`<img alt="isosceles" width="68" height="80" src=${playerA}>`);
+    $('#move-1B').replaceWith(`<img alt="scalene" width="80" height="68" src=${playerB}>`);
+})
+
+
 
 }); // Closing the .Ready Function
+
+// function movePlayer () {
+// $('div').on('click', function (){
+//
+//     $('#move-1A').empty();
+//     $('#move-2A').replaceWith("<p id=move-2A>Move</p>");
+// });
+// }
